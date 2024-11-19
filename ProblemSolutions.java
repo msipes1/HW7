65a6328c39d077464f05de2a90fe2ed4ba76cf3f
Mary Margaret <83988411+msipes1@users.noreply.github.com>
2024-11-19 10:20:49 -0600

    Update ProblemSolutions.java

--- ProblemSolutions.java
+++ ProblemSolutions.java
@@ -1,6 +1,6 @@
 /******************************************************************
  *
- *   YOUR NAME / SECTION NUMBER
+ *   Mimi Sipes / COMP 272-002
  *
  *   This java file contains the problem solutions for the methods selectionSort,
  *   mergeSortDivisibleByKFirst, asteroidsDestroyed, and numRescueCanoes methods.
